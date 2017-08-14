e891c15c6a9374070c40e54f5fcb19bb5f18c677
Caleb Severn <calsev@gmail.com>
2017-08-14 14:16:43 -0700

    Upgraded deprecated define_runner_method->define_matcher

--- libraries/dns_matcher.rb
+++ libraries/dns_matcher.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 if defined?(ChefSpec)
-  ChefSpec::Runner.define_runner_method(:dns_client)
+  ChefSpec.define_matcher(:dns_client)
 
   def set_server_ips_dns_client(resource)
     ChefSpec::Matchers::ResourceMatcher.new(:dns_client, :set_server_ips, resource)
   end
 
-  ChefSpec::Runner.define_runner_method(:dns_suffix)
+  ChefSpec.define_matcher(:dns_suffix)
 
   def set_suffix_dns_suffix(resource)
     ChefSpec::Matchers::ResourceMatcher.new(:dns_client, :set_server_ips, resource)
