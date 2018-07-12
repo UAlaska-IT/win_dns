dc1fdf9c8f74dd9da598301c8e8a9ce1cf07e7f4
calsev <calsev@gmail.com>
2018-07-12 11:46:22 -0800

    Added a real test for DNS settings

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,3 +5,12 @@ describe host('google.com') do
   it { should be_resolvable }
   it { should be_reachable }
 end
+
+script = <<-SCRIPT
+  Get-WmiObject win32_NetworkAdapterConfiguration | Select DnsServerSearchOrder, DnsDomain
+SCRIPT
+
+describe powershell(script) do
+  its('stdout') { should match '137.229.15.5, 137.229.15.9, 8.8.8.8' }
+  its('stdout') { should match 'alaska.edu' }
+end
