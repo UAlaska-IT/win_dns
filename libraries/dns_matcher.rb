7ae5b0a7592bcabcb82fefb977dbdb06ab03bed1
Caleb Severn <calsev@gmail.com>
2018-08-25 08:21:07 -0800

    Fixed matcher for dns_suffix

--- libraries/dns_matcher.rb
+++ libraries/dns_matcher.rb
@@ -10,6 +10,6 @@ if defined?(ChefSpec)
   ChefSpec.define_matcher(:dns_suffix)
 
   def set_suffix_dns_suffix(resource)
-    ChefSpec::Matchers::ResourceMatcher.new(:dns_client, :set_server_ips, resource)
+    ChefSpec::Matchers::ResourceMatcher.new(:dns_suffix, :set_suffix, resource)
   end
 end
